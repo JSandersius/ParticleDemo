c4e14c817eb24777b2e6ef0aecff8a3ac668ebbd
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-26 10:03:25 -0700

    notes

--- notes.d
+++ notes.d
@@ -1,2 +1,10 @@
 URL to this exercize
 https://www.youtube.com/watch?v=qK3cgD09Qf0
+
+This is a full run-through of particles.js, a js library
+
+configurable json file
+
+login form over partcicles background
+
+ using chrome, you're going to want some sort of server and not just open the html file in browser
